9c7b4c17d48e9204aaa4088feba5f29123b389a0
Vikrant Pradhan <pradhan.vikrant@hotmail.com>
2025-12-09 18:45:33 +0530

    Update test_members.py to remove manual arguments for username and password,
    This lets it automatically generate fake values. This also makes the
    code more readable in my opinion.

--- backend/tests/test_members.py
+++ backend/tests/test_members.py
@@ -4,20 +4,27 @@ from app.models import Group, User
 
 
 def test_create_member_fail(user_factory, auth_client_factory):
-    """Test should fail to create a member if user is not a member of the group"""
-    user: User = user_factory("Vikrant", "Pradhan")
-    user_to_add: User = user_factory("Jennie", "Kim")
+    """It should fail to create a member if user is not a member of the group"""
+
+    user: User = user_factory()
+    user_to_add: User = user_factory()
+
     client: TestClient = auth_client_factory(user.id)
-    group_id = 1  # Some group that user is not part of
-    response = client.post(f"/groups/{group_id}/members", json={"id": user_to_add.id})
+    non_existing_group_id = 1  # Some group that user is not part of
+    response = client.post(
+        f"/groups/{non_existing_group_id}/members", json={"id": user_to_add.id}
+    )
     assert response.status_code == 403
 
 
 def test_create_member_success(user_factory, auth_client_factory, group_factory):
-    """Test should succeed to create member if user is a member of the group"""
-    user: User = user_factory("Vikrant", "Pradhan")
-    user_to_add: User = user_factory("Jennie", "Kim")
+    """It should succeed to create member if user is a member of the group"""
+
+    user: User = user_factory()
+    user_to_add: User = user_factory()
+
     client: TestClient = auth_client_factory(user.id)
     group: Group = group_factory(user_id=user.id)
+
     response = client.post(f"/groups/{group.id}/members", json={"id": user_to_add.id})
     assert response.status_code == 200
